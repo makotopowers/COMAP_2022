c8358121d6f942d8ae428c1acf3f0827c57c1ee9
Makoto Powers <makotopowers15@icloud.com>
2022-02-02 12:18:01 -0500

    same

--- comap_proj.py
+++ comap_proj.py
@@ -9,18 +9,34 @@ import sklearn.linear_model
 
 
 class FishyFishy():
-    def __init__(self, fish_data, future_pred_area, daily_sst, future_sst_daily):
+    def __init__(self, fish_data, future_pred_area, future_sst_daily):
+
+
+        '''
+        initialize.
+        '''
+
         self.optimal_temp = dict()
         self.fish_data = dict()
-        self.weeks = 8000
         self.future_pred_area = future_pred_area
         self.fish_types = []
         panic = 0
         for fish, temp, survivable_days, acceptable_range in fish_data:
             self.optimal_temp[fish] = (temp, survivable_days, acceptable_range, panic)
             self.fish_types.append(fish)
-        #self.sst_daily = np.load(daily_sst).transpose(0,2,1)
         self.future_sst_daily = np.load(future_sst_daily)
+
+
+        '''
+        set the amount of days/5 to observe.
+        '''
+
+        self.days = 8000
+
+        
+        '''
+        generate the coordinates of the docks. 
+        '''
 
         dock_coords = {}
         docks = np.zeros((future_pred_area[0]-1,future_pred_area[1]-1))
@@ -34,32 +50,17 @@ class FishyFishy():
         
         self.docks = docks
         self.dock_coords = dock_coords
-        #print(self.dock_coords)
-        #print(docks)
-        #x = np.arange(0,80)
-        #y = np.arange(0,64)
-
-        #plt.pcolormesh(x,y,docks.transpose(1,0))
-        #plt.show()
         
 
     def lin_reg(self):
+
+
+        '''
+        run a linear regression for each coordinate and make a new array that holds the prediction extended to 40000 days. 
+        '''
+
         x = np.arange(14245).reshape(-1,1)
-        #content = [np.zeros((40000,1,1))]*(future_pred_area[0]*future_pred_area[1])
-        #y = self.sst_daily[x,80,64].reshape(-1,1)
-
-        #regr = sklearn.linear_model.LinearRegression()
-        #regr.fit(x,y)
         x_future = np.arange(40000).reshape(-1,1)
-        #y_future = regr.predict(x_future).reshape(-1,)
-        #print(regr.predict(np.arange(10,15).reshape(-1,1)))
-        #print(regr.predict(np.arange(2986,2988).reshape(-1,1)))
-        #plt.plot(x,y)
-        #plt.plot(x_future,y_future, color='black')
-        #plt.show()
-
-
-        #Code has already been run. Get array from self.future_sst
         
         content = [np.zeros((40000,1,1))]*(future_pred_area[0]*future_pred_area[1])
         future_sst_daily = np.array(content).reshape(40000,future_pred_area[0],future_pred_area[1])
@@ -76,12 +77,7 @@ class FishyFishy():
                 future_sst_daily[:,i,j] = y_future
 
         self.future_sst_daily = future_sst_daily
-        #np.save('/Users/makotopowers/Desktop/COMAP_2022_files',future_sst_daily)
-        print(self.future_sst_daily.shape)
-        print(self.future_sst_daily[0,25,20])
-        print(self.future_sst_daily[10000,25,20])
-        print(self.future_sst_daily[20000,25,20])
-        print(self.future_sst_daily[38000,25,20])
+        np.save('/Users/makotopowers/Desktop/COMAP_2022_files',future_sst_daily)
         plt.plot(x,y)
         plt.plot(x_future,y_future, color='black')
         plt.show()
@@ -89,6 +85,12 @@ class FishyFishy():
 
 
     def heat_map(self):
+
+
+        '''
+        display heatmap of ocean temps.
+        '''
+
         x = np.arange(0,future_pred_area[0])
         y = np.arange(0,future_pred_area[1])
         plt.pcolormesh(x,y,self.future_sst_daily[0,0:future_pred_area[0],0:future_pred_area[1]].transpose(1,0),vmin=0)
@@ -99,14 +101,19 @@ class FishyFishy():
 
     def fish_migration(self):
         
-        #initiate arrays for school position and water temp 
-        week = 1
-        water_temp = self.future_sst_daily[week*5,0:future_pred_area[0],0:future_pred_area[1]] 
+        '''
+        initiate arrays for school position and water temp 
+        '''
+
+        day = 1
+        water_temp = self.future_sst_daily[day*5,0:future_pred_area[0],0:future_pred_area[1]] 
         too_hot_counter = 0
         
-        
 
-        #initiate schools 
+        '''
+        initiate schools 
+        '''
+
         for i in range(1,1000): #how many schools
             ax = np.random.random(1)*(future_pred_area[0]-1)//1 
             ay = np.random.random(1)*(future_pred_area[1]-1)//1 
@@ -118,20 +125,17 @@ class FishyFishy():
                 ay = ay+1
             elif ay ==future_pred_area[1]-1:
                 ay = ay-1
- 
             x,y = int(ax), int(ay)
 
             random_type = np.random.choice(self.fish_types)
-
             self.fish_data[f'school_{i}'] = [x,y,too_hot_counter, random_type, self.optimal_temp[random_type][0], \
                 self.optimal_temp[random_type][1], self.optimal_temp[random_type][2], self.optimal_temp[random_type][3]]
-
-            
         print(f'FISH_DATA: {self.fish_data}')
 
-        #done initiating
 
-        #display initial positions
+        '''
+        display initial positions 
+        '''
 
         x = np.arange(0,future_pred_area[0])
         y = np.arange(0,future_pred_area[1])
@@ -139,18 +143,17 @@ class FishyFishy():
         initial_pos = np.zeros((future_pred_area[0]-1,future_pred_area[1]-1))
         for school in self.fish_data:
             initial_pos[self.fish_data[school][0],self.fish_data[school][1]] = 1
-
-        print(initial_pos.shape)
-        print(self.future_sst_daily.shape)
-        
-        overlay_1 = self.future_sst_daily[week,0:future_pred_area[0]-1,0:future_pred_area[1]-1] + 20 * initial_pos
+        overlay_1 = self.future_sst_daily[day,0:future_pred_area[0]-1,0:future_pred_area[1]-1] + 20 * initial_pos
         #plt.pcolormesh(x,y,overlay_1.transpose(1,0),vmin=-2, vmax=23)
         #plt.colorbar()
         #plt.show()
         
-        water_temp = self.future_sst_daily[int(week*5),0:future_pred_area[0],0:future_pred_area[1]] 
-            #change the position of each school 
+        
+        ''' 
+        move fish according to logic for 4*100 days before incrementing temperature.
+        '''
 
+        water_temp = self.future_sst_daily[int(day*5),0:future_pred_area[0],0:future_pred_area[1]] 
         for i in range(100):
             print(f'{i+1}/100---')
             mid_pos = np.zeros((future_pred_area[0]-1,future_pred_area[1]-1))
@@ -160,7 +163,12 @@ class FishyFishy():
             for school in self.fish_data.keys():
                 lat, lon = self.fish_data[school][0], self.fish_data[school][1]  
 
-            #move fish to block with minimum difference between optimal and temp
+
+                '''
+                move fish to block with minimum difference between optimal and temp. check position array, if there is already a fish there do not move. 
+                if fish gets stuck into a local minimum put it into panic mode. make sure fish do not leave the array. 
+                '''
+
                 try:
                     if self.fish_data[school][7] != 0:
                         self.fish_data[school][0] += int(round(random.uniform(-1,1),0))
@@ -191,30 +199,42 @@ class FishyFishy():
                     if self.fish_data[school][1] < 1:
                         self.fish_data[school][1] += 1
 
-
                 except ValueError:
                     continue
+
+
+        '''
+        create an array that holds the positions of all the fish. print. this shows where the fish would start. 
+        '''
 
         mid_pos = np.zeros((future_pred_area[0]-1,future_pred_area[1]-1))
         for school in self.fish_data:
             mid_pos[self.fish_data[school][0],self.fish_data[school][1]] = 1
 
         
-        overlay_2 = self.future_sst_daily[week,0:future_pred_area[0]-1,0:future_pred_area[1]-1] + 20 * mid_pos
+        overlay_2 = self.future_sst_daily[day,0:future_pred_area[0]-1,0:future_pred_area[1]-1] + 20 * mid_pos
         #plt.pcolormesh(x,y,overlay_2.transpose(1,0),vmin=-2, vmax=23)
         #plt.colorbar()
         #plt.show()
 
 
+        '''
+        initiate the out of range dictionary and map that represents the out of range matrix
+        '''
+
         out_of_range = {}
         out_of_range_map = np.zeros((future_pred_area[0]-1,future_pred_area[1]-1))  
 
 
-        #loop through time interval 
-        for week in range(self.weeks-1):
-            print(f'Week: {week+1}/{self.weeks} :'+'*'*int(((week+1)/self.weeks)*20//1))
-            water_temp = self.future_sst_daily[int(week*5),0:future_pred_area[0],0:future_pred_area[1]] 
-            #change the position of each school 
+        '''
+        loop through the days. set the water temp to the according water temp by copying the matrix. move fish according to logic. 
+        check the condition of the fish; if it is too hot they die. move fish 3 times for each time increment. 
+        check if the fish are out of range, for each dock. update the out of range dock data. 
+        '''
+
+        for day in range(self.days-1):
+            print(f'Day: {day+1}/{self.days} :'+'*'*int(((day+1)/self.days)*20//1))
+            water_temp = self.future_sst_daily[int(day*5),0:future_pred_area[0],0:future_pred_area[1]] 
 
             mid_pos = np.zeros((future_pred_area[0]-1,future_pred_area[1]-1))
             for school in self.fish_data:
@@ -225,7 +245,6 @@ class FishyFishy():
             for school in self.fish_data.keys():
                 lat, lon = self.fish_data[school][0], self.fish_data[school][1]  
 
-            #move fish to block with minimum difference between optimal and temp
                 try:
                     if self.fish_data[school][7] != 0:
                         self.fish_data[school][0] += int(round(random.uniform(-1,1),0))
@@ -245,7 +264,7 @@ class FishyFishy():
                             self.fish_data[school][0] += (itemindex[0][0]-1)
                             self.fish_data[school][1] += (itemindex[1][0]-1)
                         if (itemindex[0][0]-1) == 0 and (itemindex[1][0]-1) == 0 and self.fish_data[school][7] == 0:
-                            self.fish_data[school][7] = 10 #have the fish panic for 10 days
+                            self.fish_data[school][7] = 10 
                     
                     if self.fish_data[school][0] > future_pred_area[0]-2:
                         self.fish_data[school][0] = future_pred_area[0]-2
@@ -260,17 +279,13 @@ class FishyFishy():
                 except ValueError:
                     continue
 
-                #check condition of school
                 
                 if water_temp[self.fish_data[school][0], self.fish_data[school][1]] > (self.fish_data[school][4] + self.fish_data[school][6]):
                     self.fish_data[school][2] += 1
                 else:
                     self.fish_data[school][2] = 0
-
                 if self.fish_data[school][2] > self.fish_data[school][5]:
                     killed.append(school)
-                    #print(f'{school} was killed.')
-
             if killed:
                 for death in killed:
                     self.fish_data.pop(death)
@@ -282,45 +297,36 @@ class FishyFishy():
             mid_pos = np.zeros((future_pred_area[0]-1,future_pred_area[1]-1))
             for school in self.fish_data:
                 mid_pos[self.fish_data[school][0],self.fish_data[school][1]] =1
-            
-            
-
-            
-        
             for dock in self.dock_coords:
                 x,y = self.dock_coords[dock]
-                
                 if np.sum(mid_pos[max(x-dock_range,0):min(x+dock_range+1,future_pred_area[0]),max(y -dock_range,0):min(y+dock_range+1, future_pred_area[1])])<4:
                     try:
-                        out_of_range[dock]=min(out_of_range[dock], week)
+                        out_of_range[dock]=min(out_of_range[dock], day)
                     except KeyError:
-                        out_of_range[dock] = week
+                        out_of_range[dock] = day
                 else:
-                    out_of_range[dock] = week
+                    out_of_range[dock] = day
 
 
-        #print(out_of_range)
+        '''
+        display the plot for how long it takes fish to move out of range.
+        '''
+
         for dock in out_of_range:
-            
-            
             x,y =self.dock_coords[dock]
             out_of_range_map[x,y] = out_of_range[dock]
-                        
-        
-        print(out_of_range)
         
         x = np.arange(0,future_pred_area[0])
         y = np.arange(0,future_pred_area[1])
-        
         plt.pcolormesh(x,y,out_of_range_map.transpose(1,0),vmin=0,vmax=2000)
         plt.colorbar()
         plt.show()
             
 
-        
+        '''
+        display the final position.
+        '''
 
-
-        #display final position
         final_pos = np.zeros((future_pred_area[0]-1,future_pred_area[1]-1))
         try:
             for school in self.fish_data:
@@ -331,35 +337,45 @@ class FishyFishy():
         except IndexError:
             print(self.fish_data[school][0],self.fish_data[school][1])
 
-        overlay_2 = self.future_sst_daily[week,0:future_pred_area[0]-1,0:future_pred_area[1]-1] + 20 * final_pos
+        overlay_2 = self.future_sst_daily[day,0:future_pred_area[0]-1,0:future_pred_area[1]-1] + 20 * final_pos
         #plt.pcolormesh(x,y,overlay_2.transpose(1,0), vmin=-2, vmax=23)
         #plt.show()
-        
 
-
-
-    
-            
-
-
-
-        
 
    
-#ocean_data = 'sst.wkmean.1990-present.nc'
-fish_data = [('mackerel', 9, 7, 3)]
-#fish_data = [('herring', 16, 7, 3)]
+'''
+files.
+'''
 
-#('mackerel', 9, 4, 3)
-#('herring', 16, 3, 2)
-future_pred_area = [80,64]
-future_sst = 'future_sst.npy'
-daily_sst = 'daily_ssts.npy'
+#ocean_data = 'sst.wkmean.1990-present.nc'
+#future_sst = 'future_sst.npy'
+#daily_sst = 'daily_ssts.npy'
+
 future_sst_daily = 'future_sst_daily.npy'
 
-fish = FishyFishy(fish_data, future_pred_area, daily_sst, future_sst_daily)
+
+'''
+fish data.
+'''
+
+fish_data = [('mackerel', 9, 7, 3)]
+#fish_data = [('herring', 16, 7, 3)]
+#('mackerel', 9, 4, 3)
+#('herring', 16, 3, 2)
 
 
+'''
+future area of prediction. 
+'''
+
+future_pred_area = [80,64]
+
+
+'''
+instantiate.
+'''
+
+fish = FishyFishy(fish_data, future_pred_area, future_sst_daily)
 
 fish.fish_migration()
 #fish.lin_reg()
